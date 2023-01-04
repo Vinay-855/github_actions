0fa8b3cce86028d52e592e86b3f8d2df0838a3f5
Vinay-855 <101859487+Vinay-855@users.noreply.github.com>
2023-01-04 18:41:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "github_actions_secret" "example_secret" {
-  repository       = " Vinay-855/github_actions"
-  secret_name      = "AWS_ACCESS_KEY_ID"
-}
