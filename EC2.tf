1cf4233437e9c8027f6e7fad66795a19c970bf7f
Vinay-855 <101859487+Vinay-855@users.noreply.github.com>
2023-02-01 12:54:15 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -2,11 +2,12 @@ resource "aws_vpc" "customvpc" {
   cidr_block = "10.10.0.0/16"
 
   tags = {
-    "Name" = "Test-Custom-VPC"
+    "Name" = "${var.app_name}-VPC"
+    "Environment" = var.app_environment
   }
 }
 
-resource "aws_instance" "testEC2" {
-  ami           = "ami-0aa7d40eeae50c9a9"
-  instance_type = "t2.micro"
-}
+#resource "aws_instance" "testEC2" {
+#  ami           = "ami-0aa7d40eeae50c9a9"
+#  instance_type = "t2.micro"
+#}
