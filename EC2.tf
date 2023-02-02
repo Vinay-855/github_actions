c4294049d8e6468e50723cf3d9a7ef565db638d4
Vinay-855 <101859487+Vinay-855@users.noreply.github.com>
2023-02-02 14:25:16 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -16,8 +16,8 @@ resource "aws_internet_gateway" "aws-igw" {
 }
 
 
-resource "aws_instance" "testEC2" {
-  ami           = "ami-0aa7d40eeae50c9a9"
-  instance_type = "t2.micro"
-}
+#resource "aws_instance" "testEC2" {
+#  ami           = "ami-0aa7d40eeae50c9a9"
+#  instance_type = "t2.micro"
+$}
 
